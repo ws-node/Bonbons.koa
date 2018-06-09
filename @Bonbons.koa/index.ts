04bee07179bd7909ec6e3417e9fbe07c2455841d
Big Mogician <sunaoran@youzan.com>
2018-06-09 13:02:10 +0800

    init : project

---
+++ @Bonbons.koa/index.ts
@@ -0,0 +1,9 @@
+import * as Koa from "koa";
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = "hello koa2";
+});
+
+app.listen(3000);
+console.log("[demo] start-quick is starting at port 3000");
