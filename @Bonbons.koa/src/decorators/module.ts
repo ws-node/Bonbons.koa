d6fae7581c453e0b71b8c401c8816d2ce9a3e1bb
Big Mogician <sunaoran@youzan.com>
2018-06-12 17:17:59 +0800

    add : @BonbonsApp alias BKoa

--- @Bonbons.koa/src/decorators/module.ts
+++ @Bonbons.koa/src/decorators/module.ts
@@ -2,6 +2,15 @@ import { BonbonsServerConfig } from "../metadata/core";
 import { IConstructor } from "../metadata/base";
 import { BonbonsServer, BaseApp } from "../core/server";
 
+/**
+ * Create a Bonbons.koa App server
+ * ---
+ * @description
+ * @author Big Mogician
+ * @export
+ * @param {BonbonsServerConfig} config
+ * @returns
+ */
 export function BonbonsApp(config: BonbonsServerConfig) {
   return function <T extends BaseApp>(target: IConstructor<T>) {
     const theStartup = target.prototype.start;
@@ -12,3 +21,6 @@ export function BonbonsApp(config: BonbonsServerConfig) {
     };
   };
 }
+
+/** Create a Bonbons.koa App server */
+export const BKoa = BonbonsApp;
